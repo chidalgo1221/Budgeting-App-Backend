3357b05fb09f33a69b23ce8aa99562721a69c779
chidalgo1221 <carlos.hidalgo2017@mesacharter.org>
2021-06-20 21:09:21 -0400

    Updated title

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use("/transactions", transactions)
 
 app.get("/", (req, res) => {
-  res.send("Budgeting App");
+  res.send("Budgeting-App");
 });
 
 app.get("*", (req, res) => {
